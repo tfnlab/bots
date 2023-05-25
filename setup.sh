6305c566e72eeaee2b72f96e2e9435f047a6d9eb
Anita Rostomyan <info@tfnlab.com>
2023-05-25 14:39:16 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo 'hello world'
 # Update package lists
+
 sudo apt update
+sudo apt -y install default-jre
+
+sudo apt update
+
 
 # Download Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -13,9 +18,15 @@ sudo apt install wget
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Install dependencies (if any)
-sudo apt-get install -f
+sudo apt-get -y install -f
 
 # Check Google Chrome version
 google-chrome --version
 
 echo google-chrome --version
+
+export DISPLAY=:0
+
+sudo apt-get -y install xvfb
+
+xvfb-run -a your-selenium-command
